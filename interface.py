56b91747ccd14f39a6724323cdc4841d5cd4451d
cairo <cairoeth@protonmail.com>
2022-06-25 19:21:28 -0400

    Example ABI + Flask Interface Setup

---
+++ interface.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import flask
+from flask import Flask, request, render_template
+
+
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__))))
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    name = request.args.get('name')
+    hash = request.args.get('hash')
+    escrow = request.args.get('escrow')
+    signature = request.args.get('signature')
+
+    return render_template('base.html', name=name, hash=hash, escrow=escrow, signature=signature)
+
+
+if __name__ == '__main__':
+    os.environ['FLASK_ENV'] = 'development'
+    app.run(host='localhost', port=8080, debug=False)
