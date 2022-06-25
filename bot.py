aed1299b13df2b047641c5939793d12798f25df0
cairo <cairoeth@protonmail.com>
2022-06-25 15:13:39 -0400

    Metadata create logic with IPFS

--- bot.py
+++ bot.py
@@ -1,5 +1,24 @@
 import steam
 from decouple import config
+import requests
+from datetime import timedelta
+
+
+# Insert your own https://steamapis.com API key
+steam_apikey = config("steam_apikey")
+
+
+def create_metadata(data, asset_link, escrow):
+    # TODO: Create JSON metadata and upload to IPFS
+    return True
+
+
+async def process_escrow(resp):
+    their_escrow = resp["response"].get("their_escrow")
+    if their_escrow is None:  # private
+        return None
+    seconds = their_escrow["escrow_end_duration_seconds"]
+    return timedelta(seconds=seconds) if seconds else None
 
 
 class MyClient(steam.Client):
@@ -18,7 +37,17 @@ class MyClient(steam.Client):
     async def on_trade_accept(self, trade: steam.TradeOffer):  # we accepted a trade
         print(f"Accepted trade: #{trade.id}")
         await trade.accept()
-        # TODO: Get received items and create a metadata from the asset to upload it to IPFS and then mint
+        # TODO: Test if it works!
+        escrow = process_escrow(self.user._state.http.get_user_escrow(self.user.id64, trade.token))
+        items_received = trade.items_to_receive
+        market_hash_name = items_received[0].name
+        appid = items_received[0].game.id
+        contextid = items_received[0].game.context_id
+        asset_id = items_received[0].asset_id
+        asset_link = 'https://steamcommunity.com/id/{}/inventory/#{}_{}_{}'.format(self.user, appid, contextid, asset_id)
+
+        url = 'https://api.steamapis.com/market/item/{}/{}?api_key={}'.format(appid, market_hash_name, steam_apikey)
+        data = requests.get(url=url).json()
         await trade.partner.send("Asset ready to mint")
 
 
